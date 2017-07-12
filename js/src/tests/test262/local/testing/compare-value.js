78be12004d6313017621e36c0b423825e9e2b749
Leo Balter <leonardo.balter@gmail.com>
2017-07-12 11:03:34 -0400

    mock files

---
+++ js/src/tests/test262/local/testing/compare-value.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2017 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+esid: pending
+description: reportCompare
+---*/
+
+var a = 42;
+
+reportCompare(0, 0);
