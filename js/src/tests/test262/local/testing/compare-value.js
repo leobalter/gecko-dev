c5b1878c976014a5dc0c0bc89cbb7e7630f9680a
Leo Balter <leonardo.balter@gmail.com>
2017-07-13 17:14:24 -0400

    mock files

--- js/src/tests/test262/local/testing/compare-value.js
+++ js/src/tests/test262/local/testing/compare-value.js
@@ -8,4 +8,29 @@ description: reportCompare
 
 var a = 42;
 
+reportCompare(0, 0)
 reportCompare(0, 0);
+reportCompare(0, 0, "ok");
+reportCompare(true, true);
+reportCompare(true, true, "ok");
+
+reportCompare(42, foo);
+reportCompare(foo, bar);
+reportCompare(call(x), call(y));
+
+reportCompare(0, 0); // with comment
+reportCompare(0, 0, "ok"); // with comment
+reportCompare(true, true); // with comment
+reportCompare(true, true, "ok"); // with comment
+
+reportCompare(42, foo); // with comment
+reportCompare(foo, bar); // with comment
+reportCompare(call(x), call(y)); // with comment
+
+// Blocks:
+if (true) {
+    reportCompare(0, 0); // this was a reportCompare Line
+    reportCompare(foo, 42, "message");
+}
+
+reportCompare(0, 0); // at the EOF, no \n
