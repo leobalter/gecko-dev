aebea44b78d009763d0365ae335067598eade6cb
Leo Balter <leonardo.balter@gmail.com>
2017-07-31 13:53:08 -0400

    Dont merge. Samples for use with the export script

---
+++ js/src/tests/test262/export/reftest-error-syntaxerror.js
@@ -0,0 +1,20 @@
+// Copyright (C) 2017 Mozilla Corporation. All rights reserved.
+// This code is governed by the BSD license found in the LICENSE file.
+
+/*---
+description: |
+  Outside AsyncFunction, |await| is a perfectly cromulent LexicalDeclaration variable
+  name.  Therefore ASI doesn't apply, and so the |0| where a |=| was expected is a
+  syntax error.
+author: Jeff Walden <jwalden+code@mit.edu>
+features: []
+negative:
+  phase: early
+  type: SyntaxError
+esid: sec-let-and-const-declarations
+---*/
+
+function f() {
+    let
+    await 0;
+}
